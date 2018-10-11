20bc236cc6a8ac43845f822b09a65f055e72906d
0AndyZhou0 <azhou4@stuy.edu>
2018-10-10 22:24:34 -0400

    fixed add to front

--- list.c
+++ list.c
@@ -5,23 +5,30 @@
 //struct node { int i; struct node *next;};
 
 void print_list(struct node * current){
-  printf("%d\n", current -> i);
+  printf("[%d", current -> i);
   while(current = current -> next){
-    printf("%d\n", current -> i);
+    printf("\n%d", current -> i);
   }
+  printf("]\n");
 }
 
-struct node * insert_front(struct node * node, int i){
-  struct node * new = (struct node *)malloc(8 + sizeof(struct node));
-  new -> i = i;
-  new -> next = &node;
-  return new;
+struct node * insert_front(struct node * head, int i){
+  struct node * new = malloc(sizeof(struct node));
+  new -> next = head -> next;
+  new -> i = head -> i;
+  head -> next = new;
+  head -> i = i;
+  //printf("%d\n",&head);
+  //printf("%d\n",new -> next);
+  return head;
 }
+
+struct node * free_list(struct node * node);
 
 int main(){
   struct node thing;
   thing.i = 5;
-  thing.next = NULL;
+  thing.next = 0;
   print_list(&thing);
   thing = *insert_front(&thing,4);
   print_list(&thing);
