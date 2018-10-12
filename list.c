62694b39416432f8e8b66d83e38936630b337216
Andy Zhou <azhou4@stuy.edu>
2018-10-12 15:14:09 -0400

    put functions in a seperate file

--- list.c
+++ list.c
@@ -2,36 +2,6 @@
 #include <stdlib.h>
 #include "list.h"
 
-//struct node { int i; struct node *next;};
-
-void print_list(struct node * current){
-  printf("[%d", current -> i);
-  while(current = current -> next){
-    printf(", %d", current -> i);
-  }
-  printf("]\n");
-}
-
-struct node * insert_front(struct node * head, int i){
-  struct node * new = malloc(sizeof(struct node));
-  new -> next = head -> next;
-  new -> i = head -> i;
-  head -> next = new;
-  head -> i = i;
-  //printf("%d\n",&head);
-  //printf("%d\n",new -> next);
-  return head;
-}
-
-struct node * free_list(struct node * node){
-  if(node -> next){
-    free_list(node -> next);
-    node -> i = 0;
-    node -> next = 0;
-    free(node -> next);
-  }
-  return node;
-}
 
 int main(){
   struct node thing;
--- list.h
+++ list.h
@@ -1,4 +1,29 @@
-struct node { int i; struct node * next;};
-void print_list(struct node * current);
-struct node * insert_front(struct node * node, int i);
-struct node * free_list(struct node * node);
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node { int i; struct node *next;};
+
+void print_list(struct node * current){
+  printf("[%d", current -> i);
+  while(current = current -> next){
+    printf(", %d", current -> i);
+  }
+  printf("]\n");
+}
+
+struct node * insert_front(struct node * head, int i){
+  struct node * new = malloc(sizeof(struct node));
+  new -> next = head -> next;
+  new -> i = head -> i;
+  head -> next = new;
+  head -> i = i;
+  return head;
+}
+
+struct node * free_list(struct node * node){
+  if(node -> next){
+    free_list(node -> next);
+    free(node -> next);
+  }
+  return node;
+}
